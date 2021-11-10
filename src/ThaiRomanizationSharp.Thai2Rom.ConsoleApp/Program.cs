b30bdf01519fe4f643f1ae7f8c69245f4f41bb8d
aaronamm <theeranitp@gmail.com>
2021-11-10 17:11:33 +0700

    PoC rename title tag of audio files

--- src/ThaiRomanizationSharp.Thai2Rom.ConsoleApp/Program.cs
+++ src/ThaiRomanizationSharp.Thai2Rom.ConsoleApp/Program.cs
@@ -1,14 +1,33 @@
 ﻿using System;
+using System.IO;
 
 namespace ThaiRomanizationSharp.Thai2Rom.ConsoleApp
 {
     public class Program
     {
+        private static Thai2RomService service = new Thai2RomService();
+
         public static void Main(string[] args)
         {
-            var service = new Thai2RomService();
-            var output = service.Romanize("นะจ๊ะ ไม่รู้ ไม่รู้");
-            Console.WriteLine(output);
+            const string path = @"";
+            var filePaths = Directory.GetFiles(path, "*.flac", SearchOption.AllDirectories);
+            //Wait for debugging Console.ReadKey();
+
+            foreach (var filePath in filePaths)
+            {
+                var tagFile = TagLib.File.Create(filePath);
+                string title = tagFile.Tag.Title;
+                TimeSpan duration = tagFile.Properties.Duration;
+                Console.WriteLine("Title: {0}, duration: {1}", title, duration);
+
+                var titleCharacter = title.ToCharArray();
+                titleCharacter[0] = char.ToUpper(titleCharacter[0]);
+
+                // change title in the file
+                // tagFile.Tag.Title = service.Romanize(title);
+                tagFile.Tag.Title = new string(titleCharacter);
+                tagFile.Save();
+            }
         }
     }
 }
